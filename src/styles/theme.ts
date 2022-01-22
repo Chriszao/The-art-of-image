8252524a65369ef6c735561294cbdf00524fabe2
Chriszao <chris.f.assis18@gmail.com>
2022-01-22 15:56:10 -0300

    feat(front, theme): Add theme styles

---
+++ src/styles/theme.ts
@@ -0,0 +1,23 @@
+export default {
+  font: {
+    family:
+      "Poppins, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    sizes: {
+      xsmall: '0.5rem',
+      small: '0.625rem',
+      medium: '0.75rem',
+      large: '1.125rem',
+    },
+  },
+  colors: {
+    primary: '#2F80ED',
+    secondary: '#F6F8FB',
+    mainBg: '#F2F2F2',
+    boxBg: '#FFFFFF',
+    text1: '#4F4F4F',
+    text2: '#828282',
+    text3: '#A9A9A9',
+    text4: '#BDBDBD',
+    success: '#219653',
+  },
+} as const;
