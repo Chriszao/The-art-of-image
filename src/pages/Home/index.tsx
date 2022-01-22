3fb2c8329d3008b5e85c83945922c3049c0025f8
Chriszao <chris.f.assis18@gmail.com>
2022-01-22 18:38:02 -0300

    feat(front, Home): Add ImageUploader component into Home

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,7 +1,13 @@
+import ImageUploader from 'components/ImageUploader';
+
 import { Container } from './styles';
 
 const Home = () => {
-  return <Container />;
+  return (
+    <Container>
+      <ImageUploader />
+    </Container>
+  );
 };
 
 export default Home;
